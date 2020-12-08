6b7d56ef192a643f0224d8ee5473e5074db379b5
lucaa.mendes <lucas1999mendes@gmail.com>
2020-12-08 17:59:35 -0300

    Configurado o FIREBASE para adição do banco de dados no projeto

---
+++ app/src/main/java/br/com/mendes/kostenorganisator/config/ConfigDB.java
@@ -0,0 +1,12 @@
+package br.com.mendes.kostenorganisator.config;
+
+public final class ConfigDB {
+    public final String DBNAME = "kostenorganisator";
+    public final String ATIVIDADESTABLE = "atividades";
+    public final String CATEGORIASTABLE = "categorias";
+
+    public final int CREATE_OP = 0;
+    public final int READ_OP = 1;
+    public final int UPDATE_OP = 2;
+    public final int DELETE_OP = 3;
+}
